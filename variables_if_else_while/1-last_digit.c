c4c0abc2668400cff292263725cb023b26489e5d
syou7 <y.saber@outlook.fr>
2022-10-04 03:37:26 -0700

    task 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;       
 if ((n % 10) > 5)
 {
-pprintf("Last digit of %d is %d and is greater than 5\n",
+printf("Last digit of %d is %d and is greater than 5\n",
 n, n % 10);
 }
 else if ((n % 10) < 6 && (n % 10) != 0)
