45d72d9e61db89b6b01bb435fc9d071f1bc1cea6
syou7 <y.saber@outlook.fr>
2022-10-06 13:45:03 -0700

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *add - that add two integers
+ *@a: parameter of the fonction
+ *@b: parameter of the fonction
+ *Return: the result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
