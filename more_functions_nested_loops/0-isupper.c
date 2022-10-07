7338ec43d176b538b29f195bb9cfd0c9f743c222
syou7 <y.saber@outlook.fr>
2022-10-07 07:21:40 -0700

    task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -3,10 +3,8 @@
 /**
 * _isupper - a function that checks for uppercase character
 *@c: parameter of the function
-
 *Return: 1 if c is uppercase returns 0 otherwise
 */
-
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
