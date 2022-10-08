135a822886c42f9bdd95e8e3f4cdf0718043a2e0
syou7 <y.saber@outlook.fr>
2022-10-08 04:30:27 -0700

    task 6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 /**
-*main - Prints all single digit numbers starting from 0 using only putchar 2T 
+*main - Prints all single digit
+*numbers starting from 0 using only putchar 2T
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-int n;
-n ='0';
-while (n <='9')
-{
-putchar (n);
-n++;
-}
-putchar ('\n');
-return (0);
+	int n;
+
+	n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
 }
