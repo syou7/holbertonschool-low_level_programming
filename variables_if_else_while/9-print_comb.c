289a955c88ea26c9c24343cf3daddb20ede8bdbd
syou7 <y.saber@outlook.fr>
2022-10-08 04:35:53 -0700

    task 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,22 +1,24 @@
 #include<stdio.h>
 /**
-*main - prints all possible combinations of single-digit numbers.
+*main - prints all possible combinations of single-digit numbers
 *Numbers must be separated by ,, followed by a space
 *Numbers should be printed in ascending order
-*return: always 0.
+*Return: always (0)
 */
+
 int main(void)
 {
-int combin;
-for (combin = '0'; combin <= '9'; combin++)
-{
-putchar (combin);
-if (combin != '9')
-{
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
+	int combin;
+
+	for (combin = '0'; combin <= '9'; combin++)
+	{
+		putchar (combin);
+		if (combin != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
