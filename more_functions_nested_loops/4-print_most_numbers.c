cd4f5fe8b110427792d32ade9f566ce4ec400716
syou7 <y.saber@outlook.fr>
2022-10-09 09:09:46 -0700

    task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - print_most_numbers, not print 2 and 4
+ *
+ *Return :void
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n != 2 && n != 4)
+			_putchar(n + '0');
+	}
+	_putchar('\n');
+}
