b663fff27a0720b440403bdbeebe9740c925d45f
syou7 <y.saber@outlook.fr>
2022-10-09 10:05:22 -0700

    task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_diagonal - function that draws a diagonal line on the terminal
+ *@n: parameter of the function
+ *
+ *Retourn: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
